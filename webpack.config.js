b5d3047cfccd97062c39ca15e7bc9be5b8e56b21
Ivan Nosov <i.k.nosov@gmail.com>
2015-10-28 11:30:13 +0200

    some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,38 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanPlugin       = require('clean-webpack-plugin');
+
 module.exports = {
-  devtool: 'sourcemap',
-  output: {
-    filename: 'bundle.js'
+  devtool: 'source-map',
+  context: __dirname + '/src',
+  entry: {
+    app: ['webpack/hot/dev-server', './app/app.js']
   },
+  output: {
+    path: './dist',
+    filename: 'bundle-[hash].js'
+  },
+  plugins: [
+    new CleanPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Angular - CRUD',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
+       { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.styl$/, loader: 'style!css!stylus' },
        { test: /\.css$/, loader: 'style!css' }
     ]
+  },
+  devServer: {
+    contentBase: './dist',
+    colors: true,
+    noInfo: false,
+    hot: true,
+    inline: false
   }
 };
