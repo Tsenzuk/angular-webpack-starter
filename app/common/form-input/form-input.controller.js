9f609a7d468442cbec252cdda4d2f7b167af249b
Tsenzuk <tsenzuk@gmail.com>
2015-12-03 15:22:38 +0200

    form-input: default input is created in controller instead of template

--- app/common/form-input/form-input.controller.js
+++ app/common/form-input/form-input.controller.js
@@ -1,27 +1,32 @@
 import angular from 'angular'
 
-class formInputController {
-  constructor($scope, $element, $attrs, $transclude) {
+class FormInputController {
+  constructor($scope, $element, $transclude, $compile) {
     this.transcluded = false;
-    console.log(angular.element('input'))
-    Object.keys(this.messages || {}).forEach((attr) => {
-      angular.element('input').attr(attr,attr);
-    });
     this.name = Math.random().toString(36).substring(7);
     this.form = $scope.$parent[$scope.$parent.vm.name];
+
     $transclude((clone) => {
       this.transcluded = !!clone.length;
+      let element = angular.element("<input />").addClass("form-control").attr({'name':this.name, 'placeholder':this.placeholder, 'ng-model':'vm.value'});
       if(this.transcluded){
+        element.hide();
         clone.each((i,element) => {
           if(element.getAttribute){
-            this.name = element.getAttribute('name')
+            this.name = element.getAttribute('name') || this.name;
           }
         });
         return;
       }
-
+      Object.keys(this.messages || {}).forEach((attr) => {
+        element.attr(attr,attr);
+      });
+      $element.find('.input').append(element);
+      $compile(element)($scope);
     });
   }
 }
 
-export default formInputController;
+FormInputController.$inject = ['$scope', '$element', '$transclude', '$compile'];
+
+export default FormInputController;
