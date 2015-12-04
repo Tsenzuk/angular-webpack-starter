cc0d231677e1f4eea928b21373127def97f79da4
Tsenzuk <tsenzuk@gmail.com>
2015-12-04 17:27:50 +0200

    form-input-match: debug console.log remove

--- app/common/form-input-match/form-input-match.component.js
+++ app/common/form-input-match/form-input-match.component.js
@@ -7,25 +7,24 @@ let formInputMatchComponent = function ($parse) {
     },
     link: function (scope, elem, attrs, ctrl) {
       if (ctrl) {
-        if(!attrs.name){
+        if (!attrs.name) {
           throw new Error('Form inputs should have name attribute');
         }
 
         let matchInput = ctrl[scope.formInputMatch];
         let thisInput = ctrl[attrs.name];
 
-        thisInput.$validators.formInputMatch = function(){
+        thisInput.$validators.formInputMatch = function () {
           return (matchInput.$viewValue === thisInput.$viewValue)
         };
 
-        scope.$watch(getMatchValue, function(){
+        scope.$watch(getMatchValue, function () {
           matchInput.$$parseAndValidate();
-          console.log(arguments)
         });
 
-        function getMatchValue(){
+        function getMatchValue() {
           var match = matchInput;
-          if(angular.isObject(match) && match.hasOwnProperty('$viewValue')){
+          if (angular.isObject(match) && match.hasOwnProperty('$viewValue')) {
             match = match.$viewValue;
           }
           return match;
