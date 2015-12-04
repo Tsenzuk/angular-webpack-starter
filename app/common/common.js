e482b121e24752fca42f43f0e6f0c7ec05b08aa9
Tsenzuk <tsenzuk@gmail.com>
2015-12-04 17:19:54 +0200

    form-input-match: attribute directive for matching between inputs

--- app/common/common.js
+++ app/common/common.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
 import formInput from './form-input/form-input';
+import formInputMatch from './form-input-match/form-input-match';
 import User from './user/user';
 import Auth from './auth/auth';
 
 let commonModule = angular.module('app.common', [
   Navbar.name,
   formInput.name,
+  formInputMatch.name,
   User.name,
   Auth.name
 ]);
