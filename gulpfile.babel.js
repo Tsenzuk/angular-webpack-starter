0fca949665427180b208a7dd25dc239caac5f3e3
Tsenzuk <tsenzuk@gmail.com>
2015-12-10 20:00:13 +0200

    gulp: component creation from template is updated; added common;

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,36 +7,57 @@ import sync     from 'run-sequence';
 import serve    from 'browser-sync';
 import rename   from 'gulp-rename';
 import template from 'gulp-template';
-import fs       from 'fs';
 import yargs    from 'yargs';
-import lodash   from 'lodash';
 
 let reload = () => serve.reload();
-let root = 'client';
+let root = 'app';
 
-// helper method for resolving paths
-let resolveToApp = (glob) => {
-  glob = glob || '';
-  return path.join(root, 'app', glob); // app/{glob}
+let resolveTo = (type) => {
+  type = type || '';
+  return function (glob) {
+    glob = glob || '';
+    return path.join(root, type, glob);
+  }
 };
 
-let resolveToComponents = (glob) => {
-  glob = glob || '';
-  return path.join(root, 'app/components', glob); // app/components/{glob}
+let toCamelCase = (val) => {
+  val = val.charAt(0).toUpperCase() + val.slice(1).toLowerCase();
+  if (!!~val.indexOf('-')) {
+    val = val.split('-').map(toCamelCase).join('');
+  }
+  return val;
+};
+
+let createComponent = (type) => {
+  let name = yargs.argv.name.replace(/([a-z])([A-Z])/g,'$1-$2').toLowerCase();
+  let parentPath = yargs.argv.parent || '';
+  let destPath = path.join(resolveTo(type)(), parentPath, name);
+
+  return gulp.src(paths.blankTemplates)
+    .pipe(template({
+      name: name,
+      upCaseName: toCamelCase(name)
+    }))
+    .pipe(rename((path) => {
+      path.basename = path.basename.replace('temp', name);
+    }))
+    .pipe(gulp.dest(destPath));
 };
 
 // map of all paths
 let paths = {
-  js: resolveToComponents('**/*!(.spec.js).js'), // exclude spec files
-  styl: resolveToApp('**/*.styl'), // stylesheets
+  js: resolveTo('components')('**/*!(.spec.js).js'), // exclude spec files
+  styl: resolveTo('app')('**/*(.styl|.scss|.sass|.css|.less)'), // stylesheets
   html: [
-    resolveToApp('**/*.html'),
+    resolveTo('app')('**/*.html'),
     path.join(root, 'index.html')
   ],
-  entry: path.join(root, 'app/app.js'),
-  output: root,
+  entry: path.join(root, 'app.js'),
+  output: './dist',
   blankTemplates: path.join(__dirname, 'generator', 'component/**/*.**')
 };
+
+let port = process.env.PORT || 3000;
 
 // use webpack.config.js to build modules
 gulp.task('webpack', () => {
@@ -47,9 +68,9 @@ gulp.task('webpack', () => {
 
 gulp.task('serve', () => {
   serve({
-    port: process.env.PORT || 3000,
+    port,
     open: false,
-    server: { baseDir: root }
+    server: {baseDir: paths.output}
   });
 });
 
@@ -59,24 +80,22 @@ gulp.task('watch', () => {
 });
 
 gulp.task('component', () => {
-  let cap = (val) => {
-    return val.charAt(0).toUpperCase() + val.slice(1);
-  };
-  let name = yargs.argv.name;
-  let parentPath = yargs.argv.parent || '';
-  let destPath = path.join(resolveToComponents(), parentPath, name);
-
-  return gulp.src(paths.blankTemplates)
-    .pipe(template({
-      name: name,
-      upCaseName: cap(name)
-    }))
-    .pipe(rename((path) => {
-      path.basename = path.basename.replace('temp', name);
-    }))
-    .pipe(gulp.dest(destPath));
+  return createComponent('components');
 });
 
-gulp.task('default', (done) => {
+gulp.task('common', () => {
+  return createComponent('common');
+});
+
+gulp.task('sync', (done) => {
   sync('webpack', 'serve', 'watch', done);
+});
+
+gulp.task('default', () => {
+  console.log('Please choose one of other tasks:');
+  console.log('webpack', 'to pack all components and place in ' + path.output);
+  console.log('serve', 'to start this webapp on port ' + port);
+  console.log('sync', 'to both webpack and serve in a row and rerun them on source changes');
+  console.log('component', 'to create new component from template.', 'You can provide component name with --name="your-component-name".','And you can provide own path to component with --parent="your/parent/path"');
+  console.log('common', 'to create new common component from template.', 'You can provide component name with --name="your-component-name".','And you can provide own path to component with --parent="your/parent/path"');
 });
