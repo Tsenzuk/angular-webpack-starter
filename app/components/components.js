acca3a5790cb59bc0991943df44be4603eeee506
Tsenzuk <tsenzuk@gmail.com>
2015-12-14 17:24:23 +0200

    home: controller provides appropriate methods for Auth factory usage

--- app/components/components.js
+++ app/components/components.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import Home from './home/home';
 import SignUp from './sign-up/sign-up';
+import SignIn from './sign-in/sign-in';
 
 let componentModule = angular.module('app.components', [
   Home.name,
-  SignUp.name
+  SignUp.name,
+  SignIn.name
 ]);
 
 export default componentModule;
--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,7 +1,14 @@
 class HomeController {
-  constructor() {
+  constructor(Auth) {
     this.name = 'home';
+    this.user = Auth;
+  }
+
+  getUser() {
+    return this.user.getUser();
   }
 }
+
+HomeController.$inject = ['Auth'];
 
 export default HomeController;
