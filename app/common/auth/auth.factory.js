4e68a58dd6fb5d087f81ce6b57699766da3e46f8
Tsenzuk <tsenzuk@gmail.com>
2015-12-14 17:20:54 +0200

    auth: added $http requests for sign-in and sign-up

--- app/common/auth/auth.factory.js
+++ app/common/auth/auth.factory.js
@@ -1,9 +1,34 @@
-let AuthFactory = function () {
+let AuthFactory = function ($http) {
   let _user;
 
   return {
+    createUser(user){
+      return $http({
+        method: 'POST',
+        url: '/sign-up/',
+        data: user
+        //headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
+      }).then(function (data) {
+        return data
+      }, function (data) {
+        throw data
+      })
+    },
+
     setUser(user) {
-      _user = user;
+      return $http({
+        method: 'POST',
+        url: '/sign-in/',
+        data: user
+        //headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
+      }).then(function (data) {
+        _user = user;
+        user.isSignedIn = true;
+        return data
+      }, function (data) {
+        _user = undefined;
+        throw data
+      })
     },
 
     getUser() {
@@ -15,5 +40,7 @@ let AuthFactory = function () {
     }
   };
 };
+
+AuthFactory.$inject = ['$http'];
 
 export default AuthFactory;
